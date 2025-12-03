87cbce678f248efaf5b27f95ea9c28bcb5317528
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2025-12-03 18:19:10 +0000

    String conversion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,48 +1,85 @@
-let num = 33
-let num2 = null
-console.log(num)
+// let num = 33
+// let num2 = null
+// console.log(num)
 
-let stringNumber = String(num)
-console.log(typeof stringNumber)
+// let stringNumber = String(num)
+// console.log(typeof stringNumber)
 
-let nullNum2 = Number(num2)
-console.log(typeof nullNum2)
-console.table([num2,nullNum2])
+// let nullNum2 = Number(num2)
+// console.log(typeof nullNum2)
+// console.table([num2,nullNum2])
 
-let undefNum = undefined
-let undefNumber = Number(undefNum)
-console.log(typeof undefNumber)
-console.table([undefNum,undefNumber])
-
-
-// let someNum = "33abc" - NaN
-let someNum = "33"
-let someNumber = Number(someNum)
-console.log(typeof someNumber)
-console.table([someNum, someNumber])
-
-/*
-"33" - 33
-"33abc" = NaN
-null = 0
-undefined = NaN
-*/
+// let undefNum = undefined
+// let undefNumber = Number(undefNum)
+// console.log(typeof undefNumber)
+// console.table([undefNum,undefNumber])
 
 
-let isLoggedIn = 2
-let isLoggedInString = "@" // if ""-false replaced by  " " - true or "abvhv" - true
-let isLoggedOut = 0
-let isBooleanLoggedIn = Boolean(isLoggedIn) 
-let isBooleanLoggedOut = Boolean(isLoggedOut) 
-let isBooleanLoggedInString = Boolean(isLoggedInString) 
-let num1 = 4
-console.log(num1)
+// // let someNum = "33abc" - NaN
+// let someNum = "33"
+// let someNumber = Number(someNum)
+// console.log(typeof someNumber)
+// console.table([someNum, someNumber])
 
-console.log(typeof isBooleanLoggedIn)
-console.table([isLoggedIn,isBooleanLoggedIn])
+// /*
+// "33" - 33
+// "33abc" = NaN
+// null = 0
+// undefined = NaN
+// */
 
-console.log(typeof isBooleanLoggedInString)
-console.table([isLoggedInString,isBooleanLoggedInString])
 
-console.log(typeof isBooleanLoggedOut)
-console.table([isLoggedOut,isBooleanLoggedOut])
+// let isLoggedIn = 2
+// let isLoggedInString = "@" // if ""-false replaced by  " " - true or "abvhv" - true
+// let isLoggedOut = 0
+// let isBooleanLoggedIn = Boolean(isLoggedIn) 
+// let isBooleanLoggedOut = Boolean(isLoggedOut) 
+// let isBooleanLoggedInString = Boolean(isLoggedInString) 
+// let num1 = 4
+// console.log(num1)
+
+// console.log(typeof isBooleanLoggedIn)
+// console.table([isLoggedIn,isBooleanLoggedIn])
+
+// console.log(typeof isBooleanLoggedInString)
+// console.table([isLoggedInString,isBooleanLoggedInString])
+
+// console.log(typeof isBooleanLoggedOut)
+// console.table([isLoggedOut,isBooleanLoggedOut])
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " vanshika"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+ 2)       // String Conversion
+console.log(1 + "2")      // String Conversion
+console.log("1" + "2")    // String Conversion
+
+console.log("1" + 2 +2)   // String Conversion if first is string
+console.log(1 + 2 + "2")  // additing of number then string if third is string
+console.log(1 + 2 + "2" + 1 + 1 + 1)    //32111
+
+console.log(3 + 4 * 5 % 3)
+
+console.log(true)
+console.log(+true)
+
+console.log(+"")
+
+let n1,n2,n3
+n1 = n2 = n3 = 2+2
+console.table([n1,n2,n3])
+
+
+let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter)  - 100
+console.log(gameCounter++)
+console.log(gameCounter++)
+console.log(gameCounter)
