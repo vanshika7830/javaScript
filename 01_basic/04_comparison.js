494f0cf6f5caab8d39df67857d2cb8116eeec6ab
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2025-12-04 18:22:18 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,20 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+console.log("2" <= 1)
+console.log("2" <= 1)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null < 0)
+console.log(null <= 0)
+console.log(null == 0)   // == doesn't check datatype
+
+console.log(undefined == 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+console.log("2" === 2)
