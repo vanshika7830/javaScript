22e9cda65787627d170674da3e67490aadd6f5fd
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2025-12-07 19:16:27 +0000

    Strings

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -12,5 +12,21 @@ console.log(gameName.__proto__)
 console.log(gameName.length)
 console.log(gameName.toUpperCase())   // returns new String
 console.log(gameName.toLowerCase())   // returns new String
+console.log(gameName.charAt(0))
+console.log(gameName.indexOf("t"))
 
-console.log(gameName)
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-13,4)
+console.log(anotherString)
+
+const newStringOne = "    vanshika    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://vanshika@google.com%20vanshika"
+console.log(url.replace("%20","-"))
+
+console.log(url.includes("hello"))
+console.log(url.split("."))
