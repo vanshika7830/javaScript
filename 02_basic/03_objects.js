ef3a7bf20dbba92f297018db28f191e2d00513ff
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2025-12-14 18:56:00 +0000

    Objects 1

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,31 @@
+// singleton - when object made with constructor
+
+// Object literals
+
+const mySym = Symbol("key1")
+const JSUser = {
+    name : "Vanshika",
+    "full name":"Vanshika Gupta",
+    age : 20,
+    [mySym]:"mykey", //consider as symbol
+    // mySym:"mykey",  //Doesn't give error but consider as string type
+    location: "Punjab",
+    email: "vanshika@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JSUser.name)
+console.log(JSUser.email)
+console.log(JSUser["full name"])
+console.log(JSUser[mySym])
+
+JSUser.email = "vanshika@hello.com"
+// Object.freeze(JSUser)  // Further changes will not be reflected
+
+JSUser.greeting = function(){
+    console.log("Hello JS User")
+}
+
+console.log(JSUser.greeting())
+console.log(JSUser.greeting)    // Anonymous
