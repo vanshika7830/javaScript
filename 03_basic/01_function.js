c8b81176d17ab695c0e58ef44eeb5c8ee3d7665e
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-02 17:33:10 +0000

    Return Keyword

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -9,11 +9,17 @@ function greetings(name){
 // greetings("Vanshika")
 
 
-function addTwoNumbers(num1, num2){
-    console.log(num1+num2)
-}
-addTwoNumbers(5,6)   // answer = 11
-addTwoNumbers(5,"a")   // answer = 5a
-addTwoNumbers(5)   // answer = NaN
-addTwoNumbers(5,null)   // answer = 5
+// function addTwoNumbers(num1, num2){
+//     console.log(num1+num2)
+// }
+// addTwoNumbers(5,6)   // answer = 11
+// addTwoNumbers(5,"a")   // answer = 5a
+// addTwoNumbers(5)   // answer = NaN
+// addTwoNumbers(5,null)   // answer = 5
 
+function addTwoNumbers(num1, num2){
+    return num1 + num2
+}
+
+const result = addTwoNumbers(6,4)
+console.log(result)
