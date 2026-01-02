c74840cacd0de36ed65f3f0f81b31afece807e33
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-02 17:31:40 +0000

    Arithmatic Operations using Function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -1,3 +1,19 @@
 function sayName(){
     console.log("Vanshika")
 }
+
+function greetings(name){
+    console.log(`Hello ${name} Good Morning!!`)
+}
+// sayName()
+// greetings("Vanshika")
+
+
+function addTwoNumbers(num1, num2){
+    console.log(num1+num2)
+}
+addTwoNumbers(5,6)   // answer = 11
+addTwoNumbers(5,"a")   // answer = 5a
+addTwoNumbers(5)   // answer = NaN
+addTwoNumbers(5,null)   // answer = 5
+
