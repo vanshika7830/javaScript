986cbae55691c7caedcd35421ed292edd5f75a8c
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-04 18:16:19 +0000

    block or local scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,16 @@
+// let a = 20
+// const b = 30
+// var c = 50
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+{
+    let a = 20
+    const b = 30
+    var c = 50
+}
+console.log(a)   // won't run
+console.log(b)   //won't run
+console.log(c)   // run - problem bcz it is out of scope and it shouldn't run
