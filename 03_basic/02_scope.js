30d749c1205aa07d7f16fe3eed5be12a94b26b31
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-04 18:18:07 +0000

    outer scope

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -11,6 +11,14 @@
     const b = 30
     var c = 50
 }
-console.log(a)   // won't run
-console.log(b)   //won't run
-console.log(c)   // run - problem bcz it is out of scope and it shouldn't run
+// console.log(a)   // won't run
+// console.log(b)   //won't run
+// console.log(c)   // run - problem bcz it is out of scope and it shouldn't run
+
+let a = 20
+const b = 30
+var c = 50
+if(a == 20){
+    a = 60;    // can be used inside bcz outer scope
+}
+console.log(b)
