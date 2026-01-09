174af33dbe60b7358c6f888cd1cf6f0a9b02c9de
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-09 17:32:39 +0000

    variable scope

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -6,11 +6,12 @@
 // console.log(b)
 // console.log(c)
 
-{
-    let a = 20
-    const b = 30
-    var c = 50
-}
+// {
+//     let a = 20
+//     const b = 30
+//     var c = 50
+// }
+
 // console.log(a)   // won't run
 // console.log(b)   //won't run
 // console.log(c)   // run - problem bcz it is out of scope and it shouldn't run
@@ -23,23 +24,33 @@
 // }
 // console.log(b)
 
-let a = 300
-const b = 1000
-var c = 10200
-if(true){
-    let a = 400
-    const b = 700
-    // var a;    //cannot use for differnt type same name inside as well as outside
-    // var b;
-    var c = 152222
-    console.log("Inner a: ",a)   //  Block level
-    console.log("Inner b: ",b)   //  Block level
-}
-console.log("Outer a: ",a)       // Global level
-console.log("Outer b: ",b)
+// let a = 300
+// const b = 1000
+// var c = 10200
+// if(true){
+//     let a = 400
+//     const b = 700
+//     // var a;    //cannot use for differnt type same name inside as well as outside
+//     // var b;
+//     var c = 152222
+//     console.log("Inner a: ",a)   //  Block level
+//     console.log("Inner b: ",b)   //  Block level
+// }
+// console.log("Outer a: ",a)       // Global level
+// console.log("Outer b: ",b)
 
 // Updation
 // Updation
 // updation
 // continuation
 // Revisiting Functions
+
+
+let a = 300
+if(true){
+    let a = 100
+    let b = 200
+    console.log("INNER: ",a)
+}
+
+console.log("OUTER: ",a)
