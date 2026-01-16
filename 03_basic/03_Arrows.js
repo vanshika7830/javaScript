28fcefb23a1c0269e6901cc0a0f6ea03812e014a
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-16 19:30:39 +0000

    Implicit return

--- 03_basic/03_Arrows.js
+++ 03_basic/03_Arrows.js
@@ -32,7 +32,10 @@ const chai =  () => {
 
 chai()
 
-const addTwo = (num1, num2) => {
-    return num1+num2
-}
+// const addTwo = (num1, num2) => {
+//     return num1+num2
+// }
+// console.log(addTwo(3,4))
+
+const addTwo = (num1, num2) => (num1 + num2)
 console.log(addTwo(3,4))
