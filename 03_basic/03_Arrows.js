0a2809a402c23b88321b520cf3fa2dcfe3c5fc6b
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-16 19:32:06 +0000

    Object in implicit

--- 03_basic/03_Arrows.js
+++ 03_basic/03_Arrows.js
@@ -37,5 +37,8 @@ chai()
 // }
 // console.log(addTwo(3,4))
 
-const addTwo = (num1, num2) => (num1 + num2)
+// const addTwo = (num1, num2) => (num1 + num2)
+// console.log(addTwo(3,4))
+
+const addTwo = (num1, num2) => ({usernname : "vanshi"})
 console.log(addTwo(3,4))
