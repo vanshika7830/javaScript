a29dd65bda414d229b9e9a3c69c5b2edbb343d64
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-25 17:53:26 +0000

    while on array

--- 05_iterations/02_whileDoWhile.js
+++ 05_iterations/02_whileDoWhile.js
@@ -3,3 +3,11 @@ while(index <= 10){
     console.log(`The value of index is ${index}`);
     index++;
 }
+
+// On array
+let myArray = ['flash', 'vanshika', 'world']
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`Array elements are ${myArray[arr]}`)
+    arr++;
+}
