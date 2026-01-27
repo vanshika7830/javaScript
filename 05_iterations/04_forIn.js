34f002d232df6d102a1ced509c89f9798fbe87a8
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-01-27 06:41:49 +0000

    forIn loop

--- 05_iterations/04_forIn.js
+++ 05_iterations/04_forIn.js
@@ -15,12 +15,12 @@ for (const key in programming) {
     //console.log(programming[key]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
-// }
+for (const key in map) {
+    console.log(key);
+}
