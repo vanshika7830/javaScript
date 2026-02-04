c38daeb9e3d95fd315432ebd9dc33ca69b08160d
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-04 20:02:22 +0000

    js

--- 07_projects/01_colorChanger/script.js
+++ 07_projects/01_colorChanger/script.js
@@ -1,24 +1,43 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
 
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
+// buttons.forEach(function (button) {
+//   console.log(button);
+//   button.addEventListener('click', function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === 'grey') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'white') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'blue') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'yellow') {
+//       body.style.backgroundColor = e.target.id;
+//     }
     
-  });
-});
+//   });
+// });
 
+const body = document.querySelector("body")
+const buttons = document.querySelectorAll("button")
+
+buttons.forEach(function (button){
+  button.addEventListener("click", function(e){
+    if(e.target.id === "grey"){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "white"){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "blue"){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "yellow"){
+      body.style.backgroundColor = e.target.id;
+    }
+  })
+})
