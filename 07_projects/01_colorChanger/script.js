c57a31cb9972156e828af5d2b37004ba3ab175ec
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-04 20:05:37 +0000

    project1

--- 07_projects/01_colorChanger/script.js
+++ 07_projects/01_colorChanger/script.js
@@ -1,29 +1,5 @@
-// const buttons = document.querySelectorAll('.button');
-// const body = document.querySelector('body');
-
-// buttons.forEach(function (button) {
-//   console.log(button);
-//   button.addEventListener('click', function (e) {
-//     console.log(e);
-//     console.log(e.target);
-//     if (e.target.id === 'grey') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'white') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'blue') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-//     if (e.target.id === 'yellow') {
-//       body.style.backgroundColor = e.target.id;
-//     }
-    
-//   });
-// });
-
 const body = document.querySelector("body")
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll(".button")
 
 buttons.forEach(function (button){
   button.addEventListener("click", function(e){
@@ -37,6 +13,12 @@ buttons.forEach(function (button){
       body.style.backgroundColor = e.target.id;
     }
     if(e.target.id === "yellow"){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "pink"){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === "green"){
       body.style.backgroundColor = e.target.id;
     }
   })
