b73f34cf2168efb991a65352f5a0dd2328c374c0
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-08 18:08:14 +0000

    changes

---
+++ 07_projects/02_BMI/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+    const suggestions = document.querySelector('#suggestions');
+    if(bmi < 18.6){
+        suggestions.innerHTML = `<span>${bmi} is under weight</span>`
+    }
+    else if(bmi > 18.6 && bmi < 24.9){
+    suggestions.innerHTML = `<span>${bmi} is normal range</span>`
+    }
+    else{
+    suggestions.innerHTML = `<span>${bmi} is over weight</span>`
+    }
+  }
+});
+
