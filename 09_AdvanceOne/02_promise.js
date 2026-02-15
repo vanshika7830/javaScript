f32ba41c483cdd716ee21b9044a8268f021ef5b4
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-15 18:12:05 +0000

    Async await

--- 09_AdvanceOne/02_promise.js
+++ 09_AdvanceOne/02_promise.js
@@ -98,6 +98,3 @@ fetch('https://api.github.com/users/hiteshchoudhary')
     console.log(data);
 })
 .catch((error) => console.log(error))
-
-// promise.all
-// yes this is also available, kuch reading aap b kro.
