efc555d7c8e8a0afe13323904d409d18e9e010d3
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-15 18:09:31 +0000

    Promise 2

--- 09_AdvanceOne/02_promise.js
+++ 09_AdvanceOne/02_promise.js
@@ -6,3 +6,17 @@ const promiseOne = new Promise(function(resolve, reject){
         resolve()
     }, 1000)
 })
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
