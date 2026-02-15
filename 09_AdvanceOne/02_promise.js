8c13944e4576a9aca7e6044c9f2e512df6d3f997
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-15 18:10:46 +0000

    Promise 4

--- 09_AdvanceOne/02_promise.js
+++ 09_AdvanceOne/02_promise.js
@@ -30,3 +30,24 @@ const promiseThree = new Promise(function(resolve, reject){
 promiseThree.then(function(user){
     console.log(user);
 })
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
