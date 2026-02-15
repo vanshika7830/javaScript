7bde319e5bfa31d08451615a986f9877a14404ef
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-15 18:11:44 +0000

    Async await

--- 09_AdvanceOne/02_promise.js
+++ 09_AdvanceOne/02_promise.js
@@ -75,3 +75,29 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
