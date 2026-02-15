130622d976522b88ed7cadb23ea609934570e78e
vanshika7830 <143698057+vanshika7830@users.noreply.github.com>
2026-02-15 18:09:00 +0000

    Promise one

---
+++ 09_AdvanceOne/02_promise.js
@@ -0,0 +1,8 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
